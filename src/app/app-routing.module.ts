7941925fcb3f6427ee7f378a74e55491ecfc768d
Youssef Gaimes <youssef@viatoile.com>
2022-04-06 15:27:27 +0000

    Planner Source Code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { WizardComponent } from './wizard/wizard.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  { path: 'home', component: NavComponent},
+  {path: 'start', component: WizardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
